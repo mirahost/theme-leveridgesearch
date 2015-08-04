d6d936dec7666a621e3d030376d740e51bb5863d
Stefan Zamfira <stefan.zamfira@osf-global.com>
2015-08-04 19:16:35 +0300

    init project : scss files/bootstrap/gulp images export

---
+++ Gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
+
+var path = {
+    scss : {
+        src : 'dev/scss/*.scss',
+        dest : 'assets/css'
+    }
+};
+
+gulp.task('sass', function(){
+    return gulp.src( path.scss.src )
+                .pipe( sourcemaps.init() )
+                .pipe( sass() )
+                .pipe( sourcemaps.write() )
+                .pipe( gulp.dest(path.scss.dest) )
+
+});
+
+
+gulp.task('watch', function(){
+    livereload.listen();
+
+    gulp.watch(path.scss.src, ['sass']);
+});
